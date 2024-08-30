81121f02aa5bf6c203a958f87baee82f97b2c327
Roger Martinez <roger.that.dev@gmail.com>
2024-08-30 15:30:20 -0700

    accessing values from params

--- src/app/blog/[blogid]/page.tsx
+++ src/app/blog/[blogid]/page.tsx
@@ -1,3 +1,11 @@
- export default function Page() {
-  return <h1>DYNAMIC AF</h1>
+const BlogPage = ({ params }) => {
+  console.log(params.blogid) // access the id in the URL
+  return (
+    <main>
+        <h1>Dynamic AF</h1>
+
+    </main>
+  )
 }
+
+export default BlogPage
