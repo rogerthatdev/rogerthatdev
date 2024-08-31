71523dcb311e3faf2d4e56e0ff2e5ab001078aa2
Roger Martinez <roger.that.dev@gmail.com>
2024-08-31 12:14:58 -0700

    add empty post page

---
+++ src/pages/post.tsx
@@ -0,0 +1,18 @@
+
+// TODO: do this
+//! really important
+//? open question
+//* important
+
+// this page will be generated a markdown file in the /posts directory
+// will be used as a template for dynamically generated pages
+
+// TODO: Make this work
+// export default function Post({ post }) {
+//   return (
+//     <div>
+//       <h1>{post.title}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//     </div>
+//   )
+// }
