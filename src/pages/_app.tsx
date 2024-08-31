fea09fc12ac6e15d5abcf1caf7e416140c6d8b04
Roger Martinez <roger.that.dev@gmail.com>
2024-08-31 11:04:35 -0700

    clean out index, add comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,10 @@
+// Purpose: Customizes the default App component that initializes every page. 
+// This file allows you to:
+
+//     Add global CSS or style imports.
+//     Maintain state or add global functionality across all pages.
+//     Implement layout wrappers or providers (e.g., for authentication, theme contexts).
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
