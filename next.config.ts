90333f2015c7266e48f662a25a4c52abefa62e20
Roger Martinez <roger.that.dev@gmail.com>
2025-07-09 21:06:18 -0700

    Integrate MDX support by updating next.config.ts to include MDX page extensions and wrapping the configuration with createMDX.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx"
+import { create } from "domain";
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+})
+
+export default withMDX(nextConfig);
